247e23df785a3e8c05436d14f809fc64a470ab32
Altmertrick <altmertricks@gmail.com>
2022-07-30 12:32:41 +0300

    fix used configureStore

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,12 @@
-import { combineReducers, legacy_createStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './redusers/todos-reducer';
 
-const rootReducer = combineReducers({
-  todosSection: todoReducer,
+const store = configureStore({
+  reducer: {
+    todosSection: todoReducer,
+  },
 });
 
-const store = legacy_createStore(rootReducer);
-console.log(store);
 declare global {
   interface Window {
     store: any;
